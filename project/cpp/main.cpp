43f559a72c609f72e0f3f250fd9f04baed3e8a3c
LuctenFinger <hityidashi@gmail.com>
2016-05-21 17:50:27 +0800

    f

--- project/cpp/main.cpp
+++ project/cpp/main.cpp
@@ -1,46 +1,101 @@
 #include<stdio.h>
-int n;//num of account
-long oa[1024];//origin account
-long ha[1024];//hashed account
+#include<stdlib.h>
+#include<conio.h>
+int n;
 
+typedef struct node node;
+
+struct node
+{
+    int v;
+    node *next;
+    node *last;
+}*head;
 
 
 int input()
 {
-    int i;
+    int tmp, i;
+    node *p, *pre = NULL;
     FILE *fp = fopen("in.txt", "r");
     fscanf(fp, "%d", &n);
+
     for (i=0; i<n; i++)
-        fscanf(fp, "%ld", &oa[i]);
+    {
+        fscanf(fp, "%d", &tmp);
+        p = (node *)malloc(sizeof(node));
+        p->v = tmp;
+        p->next = pre;
+        p->last = NULL;
+        if (i != 0)
+            pre->last = p;
+        pre = p;
+    }
+    head = p;
+
     fclose(fp);
     return 0;
 }
 
 
-int crash()
-{}
-
-
-int hashing()
+int qs(node *p, int num)
 {
-    int i;
-    for (i=0; i<n; i++)
+    node *pre = p;
+    node *x = p->next;
+    node *after, *first = p;
+    int i, SumSmall=0, SumBig=0;
+
+    for (i=0; i<num-1; i++)
     {
-        if (ha[oa[i]%1024] == 0)
-            ha[oa[i]%1024] = oa[i];
+        after = x->next;
+
+        if (x->v < p->v)
+        {
+            if (first == head)
+                head = x;
+            else
+                first->last->next = x;
+
+            if (x->next != NULL)
+                x->next->last = x->last;
+            x->last->next = x->next;
+            x->next = first;
+            x->last = first->last;
+            first->last = x;
+            first = x;
+            SumSmall++;
+        }
         else
-            crash();
+            SumBig++;
 
-
+        x = after;
     }
+    if (SumSmall > 1)
+        qs(first, SumSmall);
+    if (SumBig > 1)
+        qs(p->next, SumBig);
 
     return 0;
 }
 
+int output()
+{
+    int i;
+    FILE *fp = fopen("out.txt", "w");
+
+    for (i=0; i<n; i++)
+    {
+        fprintf(fp, "%d ", head->v);
+        head = head->next;
+    }
+    fclose(fp);
+    return 0;
+}
 
 int main()
 {
     input();
-    hashing();
+    qs(head, n);
+    output();
     return 0;
 }
